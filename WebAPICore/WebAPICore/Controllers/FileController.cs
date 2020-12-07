0ee23a0a8a3b70d9cd9b76a581af065793bbde5f
jai <jayanjai.0029@gmail.com>
2020-12-07 16:07:02 +0530

    frist commit

---
+++ WebAPICore/WebAPICore/Controllers/FileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPICore.Data;
+using WebAPICore.Models;
+using System.IO;
+
+namespace WebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult PostFile([FromForm] FileModel file)
+        {
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.FileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    file.FormFile.CopyTo(stream);
+                }
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
